2ec101c3b3b50a1edb1b60a1ea754c924ca20f68
Арсений <arseny.potapov2012@yandex.ru>
2021-12-03 08:32:25 +0800

    Добавьте файлы проекта.

---
+++ Курсовая C/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using static Курсовая_C.Particle;
+
+namespace Курсовая_C
+{
+    public partial class Form1 : Form
+    {
+        List<Particle> particles = new List<Particle>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+            
+        }
+
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState();
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black); // А ЕЩЕ ЧЕРНЫЙ ФОН СДЕЛАЮ
+                Render(g);
+            }
+
+            picDisplay.Invalidate();
+        }
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1;  // не трогаем
+                if (particle.Life < 0)
+                {
+                    // тоже не трогаем
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+                    /* конец ЭТО ДОБАВЛЯЮ  */
+
+                    // это не трогаем
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500)
+                {
+                    // а у тут уже наш новый класс используем
+                    var particle = new ParticleColorful();
+                    // ну и цвета меняем
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        private void Render(Graphics g)
+        {
+            // утащили сюда отрисовку частиц
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+        // добавляем переменные для хранения положения мыши
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            // в обработчике заносим положение мыши в переменные для хранения положения мыши
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
+        }
+    }
+}
